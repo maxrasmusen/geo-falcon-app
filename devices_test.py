56b3096fcc3750e8553b62f0d9eac7c2eca27074
Max Rasmusen <mrasmusen@spartaglobal.co>
2017-07-01 17:38:49 +0100

    added two more tests

--- devices_test.py
+++ devices_test.py
@@ -3,12 +3,21 @@ import unittest
 import colour_runner.runner
 
 class TestDevicesResponse(unittest.TestCase):
- 
-  def test_get_correct_response(self):
-    devices_request = dto.DevicesRequestWrapper()
-    self.assertEqual(devices_request.getsingledevice('ct'), 'Legacy Legato CT transmitter')
+  
+  def setUp(self):
+    self.devices_request = dto.DevicesRequestWrapper()
+
+  def test_response_when_single_line(self):
     
+    self.assertEqual(self.devices_request.get_single_device_body('ct'), 'Legacy Legato CT transmitter')
     
+  def test_response_when_multi_line(self):
+    self.assertEqual(self.devices_request.get_single_device_body('hub_ethernet_cosy_generic'), "Generic hub to connect to Cosy servers and perform fast firmware download. Used to deploy hub firmware at the point of installation.")
+
+  def test_response_when_does_not_exist(self):
+    self.assertEqual(self.devices_request.get_single_device_status('i don\'t exist'), 404)
+
+
 suite = unittest.TestLoader().loadTestsFromTestCase(TestDevicesResponse)
 colour_runner.runner.ColourTextTestRunner().run(suite)
 
--- devices_test_object.py
+++ devices_test_object.py
@@ -5,6 +5,10 @@ class DevicesRequestWrapper:
   def __init__(self):
     self.conn = httplib.HTTPConnection('localhost:8000')
 
-  def getsingledevice(self, device_name):
+  def get_single_device_body(self, device_name):
     self.conn.request("GET", "/devices", "", {"device_name": device_name})
     return self.conn.getresponse().read()
+
+  def get_single_device_status(self, device_name):
+    self.conn.request("GET", "/devices", "", {"device_name": device_name})
+    return self.conn.getresponse().status
