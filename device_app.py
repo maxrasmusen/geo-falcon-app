e9987dc35f5d05b3bceaa03658a73a2b6d976153
Max Rasmusen <mrasmusen@spartaglobal.co>
2017-07-01 16:37:38 +0100

    added xml parsers and iterate through devices

--- device_app.py
+++ device_app.py
@@ -1,10 +1,27 @@
 import falcon
+import xml.etree.ElementTree as et
 
-class ThingsResource(object): 
+class DevicesResource(object): 
+
   def on_get(self, req, res): 
+
+    # Get device name stored in request head. 
+    device_name = req.headers['DEVICE-NAME']
+    
+    # Parse xml from file and find 'devices tag'
+    devices = et.parse('mini-schema.xml')
+    devices = devices.getroot().find('devices')
+
+    # Iterate through all devices
+    for device in devices.findall('device'):
+      print(device.find('name').text)
+
     res.status = falcon.HTTP_200
     res.body = ("HELLO")
 
+# Start falcon server
 app = falcon.API()
-things = ThingsResource()
-app.add_route('/things', things)
+
+# Add instance of devices controller to app
+devices = DevicesResource()
+app.add_route('/devices', devices)
