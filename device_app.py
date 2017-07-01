48bfa6683ae723a4a0275156854ade484491b3bf
Max Rasmusen <mrasmusen@spartaglobal.co>
2017-07-01 16:44:44 +0100

    can now get notes for a device

--- device_app.py
+++ device_app.py
@@ -1,23 +1,30 @@
 import falcon
 import xml.etree.ElementTree as et
 
-class DevicesResource(object): 
-
-  def on_get(self, req, res): 
-
-    # Get device name stored in request head. 
-    device_name = req.headers['DEVICE-NAME']
-    
+def get_notes(device_name):
     # Parse xml from file and find 'devices tag'
     devices = et.parse('mini-schema.xml')
     devices = devices.getroot().find('devices')
 
     # Iterate through all devices
     for device in devices.findall('device'):
-      print(device.find('name').text)
+      d = device.find('name').text
+      if d == device_name:
+        return device.find('notes').text
 
-    res.status = falcon.HTTP_200
-    res.body = ("HELLO")
+
+class DevicesResource(object): 
+
+    def on_get(self, req, res): 
+
+      # Get device name stored in request head. 
+      device_name = req.headers['DEVICE-NAME']
+      print device_name
+      
+      notes = get_notes(device_name)
+      
+      res.status = falcon.HTTP_200
+      res.body = (notes)
 
 # Start falcon server
 app = falcon.API()
