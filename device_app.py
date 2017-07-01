9f8eb7d4f6c8a4fae10c5623b97f83e5e16c3034
Max Rasmusen <mrasmusen@spartaglobal.co>
2017-07-01 19:30:02 +0100

    moved xml reading into it's own class

--- device_app.py
+++ device_app.py
@@ -1,39 +1,18 @@
 import falcon
+import json
+import xml_reader as x
 import xml.etree.ElementTree as et
 import re
-import json
-
-def get_notes(device_name):
-    # Parse xml from file and find 'devices tag'
-    minischema = et.parse('mini-schema.xml')
-    devices = minischema.getroot().find('devices')
-
-    # Iterate through all devices. Return the notes if names match
-    for device in devices.findall('device'):
-      d = device.find('name').text
-      if d == device_name:
-        notes = device.find('notes').text
-        # Remove tabs, CRs and newlines from notes, replacing newlines with spaces. Remove extra whitespace around edge.
-        notes = strip_whitespace(notes)
-        return notes
-
-    # If we get here none have matched. 
-    return False
-
-def strip_whitespace(notes):
-  
-  notes = re.sub(r'[\t\r]', r'', notes)
-  notes = re.sub(r'\n', r' ', notes)
-  notes = notes.strip()
-        
-  return notes
 
 class DevicesResource(object): 
+
+    def __init__(self):
+      self.xml_reader = x.XMLReader()
 
     def get_single_device(self, req, res):
       device_name = req.headers['DEVICE-NAME']
         
-      notes = get_notes(device_name)
+      notes = self.xml_reader.get_single_device_notes(device_name)
       
       if notes: 
         res.status = falcon.HTTP_200
@@ -49,7 +28,7 @@ class DevicesResource(object):
       for device in devices.findall('device'):
         name = device.find('name').text
         value = device.find('value').text
-        notes = strip_whitespace(device.find('notes').text)
+        notes = self.xml_reader.strip_whitespace(device.find('notes').text)
         dict = {'name': name, 'value': value, 'notes': notes}
         print(dict)
         all_devices.append(dict)
---
+++ xml_reader.py
@@ -0,0 +1,31 @@
+import xml.etree.ElementTree as et
+import re
+
+class XMLReader():
+
+  def __init__(self):
+    minischema = et.parse('mini-schema.xml')
+    self.devices = minischema.getroot().find('devices')
+
+  def get_single_device_notes(self, device_name):
+      
+      # Iterate through all devices. Return the notes if names match
+      for device in self.devices.findall('device'):
+        d = device.find('name').text
+        if d == device_name:
+          notes = device.find('notes').text
+          # Remove tabs, CRs and newlines from notes, replacing newlines with spaces. Remove extra whitespace around edge.
+          notes = self.strip_whitespace(notes)
+          return notes
+
+      # If we get here none have matched. 
+      return False
+
+
+  def strip_whitespace(self, notes):
+    
+    notes = re.sub(r'[\t\r]', r'', notes)
+    notes = re.sub(r'\n', r' ', notes)
+    notes = notes.strip()
+          
+    return notes
